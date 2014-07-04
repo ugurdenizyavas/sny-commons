c758bf91a2c9fbe6fd0c7a1cd2cf58f52caff5ef
trerginl <lemiorhan.ergin@eu.sony.com>
2014-07-04 12:05:46 +0300

    added fileutils

---
+++ src/main/java/com/sony/ebs/octopus3/commons/file/FileUtils.java
@@ -0,0 +1,66 @@
+package com.sony.ebs.octopus3.commons.file;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * @author trerginl
+ * @since 04.07.2014
+ */
+public class FileUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileUtils.class);
+
+    private FileUtils() throws InstantiationException {
+        throw new InstantiationException("Utility classes should not be instantiated");
+    }
+
+    public static void deleteDirectory(Path dir) {
+        try {
+            Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    if (exc == null) {
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    } else {
+                        throw exc;
+                    }
+                }
+
+            });
+        } catch (IOException e) {
+            logger.debug("Unable to delete directory [" + dir + "] due to errors", e);
+        }
+    }
+
+    public static boolean writeFile(Path path, String content, boolean override, boolean createMissingFolders) {
+        try {
+            if (!override && Files.exists(path)) {
+                logger.debug("File already exists in path [" + path + "] and override is not allowed");
+                return false;
+            }
+            if (createMissingFolders) Files.createDirectories(path.getParent());
+            Files.write(path, content.getBytes());
+            logger.debug("File is written in path [" + path + "]");
+        } catch (IOException e) {
+            logger.debug("Content cannot be written to path [" + path + "]", e);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/sony/ebs/octopus3/commons/file/FileUtilsTest.java
@@ -0,0 +1,58 @@
+package com.sony.ebs.octopus3.commons.file;
+
+import com.sony.ebs.octopus3.commons.date.ISODateUtils;
+import org.junit.After;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class FileUtilsTest {
+
+    @Test
+    public void writeFile() throws Exception {
+        Path basePath = Paths.get(System.getProperty("java.io.tmpdir") + "/fileTest/a/b/c");
+        Path filePath = Paths.get(basePath.toString() + "/file.txt");
+        assertTrue(FileUtils.writeFile(filePath, "test", true, true));
+    }
+
+    @Test
+    public void writeFile_unableToWrite_alreadyExists() {
+        Path basePath = Paths.get(System.getProperty("java.io.tmpdir") + "/fileTest/a/b/c");
+        Path filePath = Paths.get(basePath.toString() + "/file.txt");
+        assertTrue(FileUtils.writeFile(filePath, "test", true, true));
+
+        assertFalse(FileUtils.writeFile(filePath, "test", false, true));
+    }
+
+    @Test
+    public void writeFile_unableToWrite_missingFolders() throws Exception {
+        Path basePath = Paths.get(System.getProperty("java.io.tmpdir") + "/fileTest/a/b/c");
+        Path filePath = Paths.get(basePath.toString() + "/file.txt");
+        assertFalse(FileUtils.writeFile(filePath, "test", true, false));
+    }
+
+    @After
+    public void doAfter() {
+        Path basePath = Paths.get(System.getProperty("java.io.tmpdir") + "/fileTest");
+        FileUtils.deleteDirectory(basePath);
+    }
+
+    @Test (expected = InstantiationException.class)
+    public void utilityClassCheck() throws Throwable {
+        try {
+            Constructor c = Class.forName(FileUtils.class.getName()).getDeclaredConstructor();
+            c.setAccessible(true);
+            c.newInstance();
+        } catch(InvocationTargetException e) {
+            throw e.getTargetException();
+            // no need to expect reflection errors
+            // we are interested in our own exceptions
+        }
+    }
+}
