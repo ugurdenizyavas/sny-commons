c93ba4b76c9e7315aadf80bae681796093f37a6a
trerginl <lemiorhan.ergin@eu.sony.com>
2014-07-11 14:22:23 +0300

    logback integration and deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,69 @@
 #!/bin/bash
 
-####
-#
-# Run this script from the root folder of your project
-#
-# Example usage:
-#    Deploy via vagrant: ./scripts/deploy.sh -i 43.216.132.90 -u GWTSYN_dev_rw -p 9091 -v 169.254.14.248 -j octopus3-repository-service-1.0-SNAPSHOT-fat.jar
-#    Deploy directly   : ./scripts/deploy.sh -i 43.216.132.90 -u GWTSYN_dev_rw -p 9091 -j octopus3-repository-service-1.0-SNAPSHOT-fat.jar
-#    Restart directly  : ./scripts/deploy.sh -i 43.216.132.90 -u GWTSYN_dev_rw -p 9091 -j octopus3-repository-service-1.0-SNAPSHOT-fat.jar -r
-#
-####
-
 set -e
 
 usage() {
-    echo "USAGE: deploy.sh
-     [-i || --ip <server ip>]
-     [-u || --username <username>]
-     [-p || --port <port>]
-     [-v || --vagrant <vagrant ip>]
-     [-r || --restart]
-     [-j || --jar <name of fatjar>]"
+    echo "
+NAME
+      deploy.sh - Deploys Octopus3 microservice jars to remote servers
+
+SYNOPSIS
+      deploy.sh
+            [-i || --ip <server ip>]
+            [-u || --username <username>]
+            [-p || --port <port>]
+            [-v || --vagrant <vagrant ip>]
+            [-e || --env <environment>]
+            [-l || --logDirectory <log directory>]
+            [-r || --restart]
+            [-j || --jar <name of fatjar>]
+            [-n || --name <name of the application>]
+
+DESCRIPTION
+      deploy.sh is a Bash script for deploying microservices to remote servers and run them.
+
+OPTIONS
+      -i, --ip
+          IP of the remote server
+      -u, --username
+          Username of the server
+      -p, --port
+          Port of ratpack
+      -v, --vagrant
+          If you are deploying through vagrant, this is the IP of your vagrant instance
+      -l, --logDirectory
+          The path where all log files are stored
+      -e, --environment
+          Environment to set. It is one among dev, testqa, staging or production
+      -n, --name
+          Name of the application, like octopus3-repository-service
+      -r, --restart
+          Defines if this is just a restart or not. Restart means we don't deploy the package, just restart it
+      -h, --help
+          Prints usage information for help.
+
+USAGE
+      Run this script from the root folder of your project. For example, even if you are in octopus3-repository-service
+      project folder, you can call the script like \"../octopus3-commons/scripts/deploy.sh\". I assume you already cloned
+      octopus3-commons project.
+
+      It is adviced to upload your public key to dev servers. In order to do that, follow the following steps.
+      1) [Local machive] run \"ssh-keygen\" command and create your public key. If you want to connect to dev without password
+         leave \"pass phrase\" empty when ssh-keygen asks.
+      2) [Local machine] open ~/.ssh/id_rsa.pub file and copy the line.
+      3) [Remote machine] login to a dev server. Open \"~/.ssh/authorized_keys\" file and paste the line you copied previously
+
+      Now you should connect to dev server via ssh without entering any password. You should try these steps for each
+      remote server one by one.
+
+      Example usage:
+        Deploy via vagrant: ../octopus3-commons/scripts/deploy.sh -i 43.216.132.90 -u GWTSYN_dev_rw -p 9091 -j octopus3-repository-service-1.0-SNAPSHOT-fat.jar -l /opt/logs/repository -e dev -n octopus3-repository-service -v 169.254.14.248
+        Deploy directly   : ../octopus3-commons/scripts/deploy.sh -i 43.216.132.90 -u GWTSYN_dev_rw -p 9091 -j octopus3-repository-service-1.0-SNAPSHOT-fat.jar -l /opt/logs/repository -e dev -n octopus3-repository-service
+        Restart directly  : ../octopus3-commons/scripts/deploy.sh -i 43.216.132.90 -u GWTSYN_dev_rw -p 9091 -j octopus3-repository-service-1.0-SNAPSHOT-fat.jar -l /opt/logs/repository -e dev -n octopus3-repository-service -r
+
+DISCUSSIONS
+      The script first uploads the file remote server and runs \"start.sh\" to make it deployed and started.
+"
     exit 1
 }
 
@@ -30,6 +73,9 @@ userName=
 port=
 vagrant=
 restart=
+logDirectory=
+environment=
+name=
 
 realargs="$@"
 while [ $# -gt 0 ]; do
@@ -54,6 +100,18 @@ while [ $# -gt 0 ]; do
             fatJarName=$2
             shift
         ;;
+        -e | --env)
+            environment=$2
+            shift
+        ;;
+        -l | --logDirectory)
+            logDirectory=$2
+            shift
+        ;;
+        -n | --name)
+            name=$2
+            shift
+        ;;
         -r | --restart)
             restart=true
         ;;
@@ -65,10 +123,15 @@ while [ $# -gt 0 ]; do
 done
 set -- $realargs
 
-if [ -z "$ip" ] || [ -z "$userName" ] || [ -z "$port" ] || [ -z "$fatJarName" ]; then
+if [ -z "$ip" ] || [ -z "$userName" ] || [ -z "$port" ] || [ -z "$fatJarName" ] || [ -z "$logDirectory" ] || [ -z "$environment" ] || [ -z "$name" ]; then
+    echo "[ABORTED] Mandatory fields are missing. Please check the usage."
     usage
     return
 fi
+
+echo "==============================="
+echo "DEPLOYMENT"
+echo "==============================="
 
 fatJarPath="build/libs/$fatJarName"
 
@@ -81,12 +144,17 @@ fi
 if [ -z "$vagrant" ];then
     if [ -z "$restart" ]; then
         scp $fatJarPath $userName@$ip:/opt
+        echo "[UPLOAD ] Package is uploaded to server"
     fi
-    ssh -t -x $userName@$ip "/opt/start.sh -p $port -j fatJarName"
+    ssh -t -x $userName@$ip "/opt/start.sh -p $port -l $logDirectory -e $environment -n $name -j fatJarName"
+    echo "[TRIGGER] Start script is triggered"
 else
     if [ -z "$restart" ]; then
         scp $fatJarPath vagrant@$vagrant:
+        echo "[UPLOAD ] Package is uploaded to vagrant"
         ssh -t -x vagrant@$vagrant "scp $fatJarName $userName@$ip:/opt"
+        echo "[UPLOAD ] Package is uploaded to server"
     fi
-    ssh -t -x vagrant@$vagrant "ssh -t -x $userName@$ip \"/opt/start.sh -p $port -j $fatJarName\""
+    echo "[TRIGGER] Start script is triggered"
+    ssh -t -x vagrant@$vagrant "ssh -t -x $userName@$ip \"/opt/start.sh -p $port -l $logDirectory -e $environment -n $name -j $fatJarName\""
 fi
--- scripts/remote/start.sh
+++ scripts/remote/start.sh
@@ -2,19 +2,30 @@
 
 ####
 #
-# Example usage: /start.sh -p 9091 -j octopus3-repository-service-1.0-SNAPSHOT-fat.jar
+# Example usage: ./start.sh -p 9091 -j octopus3-repository-service-1.0-SNAPSHOT-fat.jar
 #
 ####
 
 set -e
 
 usage() {
-    echo "USAGE: start.sh [-p | --port <port>] [-j | --jar <jar name>]"
+    echo " USAGE:
+    start.sh
+    [-p || --port <port>]
+    [-j || --jar <jar name>]
+    [-e || --env <environment>]
+    [-l || --logDirectory <log directory>]
+    [-n || --name <name of server>]
+    "
     exit 1
 }
 
+now=$(date +"%Y%m%d_%H%M")
 port=
 jar=
+logDirectory=
+environment=
+name=
 
 realargs="$@"
 while [ $# -gt 0 ]; do
@@ -27,6 +38,18 @@ while [ $# -gt 0 ]; do
             port=$2
             shift
         ;;
+        -e | --env)
+            environment=$2
+            shift
+        ;;
+        -l | --logDirectory)
+            logDirectory=$2
+            shift
+        ;;
+        -n | --name)
+            name=$2
+            shift
+        ;;
         *)
             usage
         ;;
@@ -35,24 +58,44 @@ while [ $# -gt 0 ]; do
 done
 set -- $realargs
 
-if [ -z "$jar" ] || [ -z "$port" ]; then
+if [ -z "$jar" ] || [ -z "$port" ] || [ -z "$logDirectory" ] || [ -z "$environment" ]; then
     usage
     return
 fi
 
 echo "==============================="
-echo "DEPLOYMENT"
+echo "START"
 echo "==============================="
 
-pid=`ps ax | grep java | grep "ratpack" | awk '{print $1}'`
-if [ pid ] && [[ $pid != '' ]]; then
-    kill -HUP $pid
-    echo "[CLEANUP] Existing instance is killed [pid:$pid]"
+pid=`ps ax | grep java | grep "ratpack" | grep "$name" | awk '{print $1}'`
+if [ -n "$pid" ]; then
+    kill -9 $pid
+    echo "[STOP   ] Existing instance is killed [pid:$pid]"
+fi
+
+pid=`ps ax | grep java | grep "ratpack" | grep "$name" | awk '{print $1}'`
+if [ -n "$pid" ]; then
+    echo "[ABORTED] Process [id:$pid] cannot be killed. Aborted."
+    exit 1
 fi
 
 echo "[START  ] Service is starting"
-nohup /opt/java/bin/java -Dratpack.port=$port -jar /opt/$jar 2>/dev/null &
+nohup /opt/java/bin/java -Dratpack.port=$port -DlogDirectory=$logDirectory -Denvironment=$environment -Dname=$name -jar /opt/$jar 2>/dev/null &
 sleep 10
-echo "[DONE   ] Service started"
+echo "[START  ] Service started"
+
+cp "/opt/${jar}" "/opt/archive/packages/${now}-${jar}"
+echo "[ARCHIVE] Package is archived to /opt/archive/packages/ directory"
+
+cp -r "$logDirectory" "/opt/archive/logs/${now}-logs"
+echo "[ARCHIVE] Logs are archived to /opt/archive/logs/ directory"
+
+pid=`ps ax | grep java | grep "ratpack" | grep "$name" | awk '{print $1}'`
+if [ -z "$pid" ]; then
+    echo "[ABORTED] Process [id:$pid] cannot be killed. Aborted."
+    exit 1
+else
+    echo "[DONE   ] Process [id:$pid] is started. Done."
+fi
 
 echo "==============================="
