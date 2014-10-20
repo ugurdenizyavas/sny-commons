652deb065ca505e44b5cce2aef35bc184b10bd9e
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-10-20 14:26:29 +0300

    Process is added

---
+++ src/main/java/com/sony/ebs/octopus3/commons/process/Process.java
@@ -0,0 +1,13 @@
+package com.sony.ebs.octopus3.commons.process;
+
+/**
+ * author: TRYavasU
+ * date: 20/10/2014
+ */
+public class Process {
+    ProcessId processId;
+    FlowTypeEnum domain;
+    ServiceTypeEnum service;
+    String publication;
+    String locale;
+}
---
+++ src/main/java/com/sony/ebs/octopus3/commons/process/ServiceTypeEnum.java
@@ -0,0 +1,9 @@
+package com.sony.ebs.octopus3.commons.process;
+
+/**
+ * author: TRYavasU
+ * date: 20/10/2014
+ */
+public enum ServiceTypeEnum {
+    DELTA, PRODUCT
+}
